cbb7c829d52e56179f5723e61089375844953ea2
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2024-07-12 20:21:59 -0700

    fix checksum

--- utilities/CheckSum.py
+++ utilities/CheckSum.py
@@ -1,5 +1,6 @@
 '''very simple checksum in metacat format'''
 import zlib
+import sys
 from io import BytesIO 
 
 def Adler32(filename):
@@ -7,4 +8,8 @@ def Adler32(filename):
     with open(filename, "rb") as fh:
         buf = BytesIO(fh.read())
         data=zlib.adler32(buf.getvalue())
-        return hex(data)[2:]
+       # return hex(data)[2:]
+        return "%08x" % data 
+
+if __name__ == "__main__":
+    print (Adler32(sys.argv[1]))
