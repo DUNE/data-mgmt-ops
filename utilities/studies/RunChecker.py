746323b980282eb84b28adc4492602a2fbad695d
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2024-07-14 09:25:51 -0700

    add in timestamp and size/file checks

--- utilities/studies/RunChecker.py
+++ utilities/studies/RunChecker.py
@@ -31,6 +31,8 @@ from TypeChecker import TypeChecker
 
 from metacat.webapi import MetaCatClient
 
+from TimeUtil import unix_to_timestamp
+
 if __name__ == '__main__':
 
     if len(sys.argv) != 3:
@@ -93,7 +95,32 @@ if __name__ == '__main__':
                 gb = int(result["total_size"]/1000./1000.)/1000.
                 result["total_size_gb"]=gb
                 result.pop("total_size")
+                result["size_per_file_gb"] = gb/result["count"]
+                if result["size_per_file_gb"] < 0.001:
+                    print ("\n WARNING VERY SMALL FILES:",run,data_stream,data_tier,result["size_per_file_gb"]*1000,"MB\n" )
                 data[run][data_stream][data_tier]=result
+
+                # look at some details
+                if data_tier in ["raw"]:
+                    query = "files where core.runs[any]=%d and core.run_type=hd-protodune and core.file_type=detector and core.data_tier=%s and core.data_stream=%s limit 1"%(run,data_tier,data_stream)
+                    try:
+                        result = list(mc_client.query(query))
+                    except:
+                        print("query failed")
+                        continue
+                    fid = result[0]["fid"]
+
+                    try:
+                        result = mc_client.get_file(fid=fid,with_metadata=True)
+                    except:
+                        print("query failed")
+                        continue
+                    if "metadata" not in result:
+                        continue
+                    if data_tier == "raw":
+                        data[run][data_stream][data_tier]["timestamp"] = unix_to_timestamp(result["metadata"]["core.start_time"])
+
+
 
         print(run, data[run])
     final = json.dumps(data,indent=4)   
@@ -103,7 +130,7 @@ if __name__ == '__main__':
     f.close()
 
     summary = []
-    fieldnames = ["run","data_stream"]
+    fieldnames = ["run","data_stream","timestamp"]
     for run in data:
         record = {}
         record["run"]=run
@@ -115,8 +142,12 @@ if __name__ == '__main__':
                 record["data_stream"]=thestream
                 #print (run, thestream, data[run][thestream])
                 for tier in data[run][thestream]:
+                     
                     #print (run, thestream, tier, data[run][thestream][tier])
                     for field in data[run][thestream][tier]:
+                        if field == "timestamp":
+                            record["timestamp"] = data[run][thestream][tier][field]
+                            continue
                         name = "%s:%s"%(tier,field)
                         value = data[run][thestream][tier][field]
                         record[name]=value
@@ -125,7 +156,7 @@ if __name__ == '__main__':
                         #print (name,value)
                 
                 summary.append(record)
-                print(run,record)
+                #print(run,record)
                     
     newfile = fname.replace("json","csv")
     with open(newfile, 'w', newline='') as csvfile:
