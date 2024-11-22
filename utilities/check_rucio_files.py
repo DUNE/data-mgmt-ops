a59bdeb9b9098926c340dda91b106f4ae60b892f
Jacob Calcutt <calcuttj@msu.edu>
2024-11-22 12:05:01 -0600

    Added more docs

--- utilities/check_rucio_files.py
+++ utilities/check_rucio_files.py
@@ -42,64 +42,83 @@ from argparse import ArgumentParser as ap
 import multiprocessing as mp
 
 def to_str(f):
+  '''Simple string output'''
   return f'{f["scope"]}:{f["name"]}'
 
-def check_one(fin):#, good_files, bad_files):
+def check_one(fin):
+  '''Check a single metacat file if it exists in rucio'''
   i, f = fin
   print(i)
   try:
+    #If it exists return true
     dc.list_content(scope=f['scope'], name=f['name'])
     return (to_str(f), True)
   except(rucio.common.exception.DataIdentifierNotFound):
+    #Return false if there's an exception -- that means it doesn't exist
     return (to_str(f), False)
     
 
 def get_files(args):
+  #Get simple dids from metacat
   files = [f for f in mc.query(args.mql)]
   return [
     {'scope':f['namespace'], 'name':f['name']} for f in files
   ]
 
 def check_content(args):
-  files = get_files(args)
+  '''Method to check whether metacat-declared files also exist in rucio'''
 
+  #Get the files from metacat
+  files = get_files(args)
   print('Got', len(files), 'files')
+
+  #Run in parallel and execute the checking
   with mp.Pool(args.n) as pool:
     results = pool.map(check_one, enumerate(files))
 
+  #Write out the list of files that do not exist in rucio
   with open(f'{args.outstr}_bad_files.txt', 'w') as f:
     f.writelines([f[0] + '\n' for f in results if not f[1]])
+
+  #Write out the list of files that do exist in rucio
   with open(f'{args.outstr}_good_files.txt', 'w') as f:
     f.writelines([f[0] + '\n' for f in results if f[1]])
 
-def check_one_replica(f):
-  print(rc.list_replicas(scope=f.split(':')[0], name=f.split(':')[1]))
-
 def check_replicas(args):
+  '''
+  Method to check whether rucio-declared files have a replica
+  '''
+
+  #Get the file list as dids
   with open(args.i, 'r') as f:
     files = [l.strip('\n') for l in f.readlines()]
-
   files = [
     {'scope':f.split(':')[0], 'name':f.split(':')[1]}
     for f in files
   ]
-  n = 1 if args.n is None else args.n
 
+  #Split into args.n chunks
+  n = 1 if args.n is None else args.n
   import math
   chunk = math.ceil(len(files)/n)
   files = [
     files[i*chunk:(i+1)*chunk] for i in range(n)
   ]
 
+  #Iterate over chunks and list replicas
   results = []
   for chunk in files:
     results += [
+        # Checks if there is an rse associated with this file
+        # and sets a flag if so
         (f'{i["scope"]}:{i["name"]}',len(i['rses']) > 0)
         for i in rc.list_replicas(dids=chunk)
     ]
 
+  #Save the files without replicas
   with open(f'{args.outstr}_no_replicas.txt', 'w') as f:
     f.writelines([f[0] + '\n' for f in results if not f[1]])
+  #Save the files with replicas
   with open(f'{args.outstr}_with_replicas.txt', 'w') as f:
     f.writelines([f[0] + '\n' for f in results if f[1]])
 
