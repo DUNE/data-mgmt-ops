b7a3e7922af402a92d53c1937263a0818c5f4dc4
Heidi Schellman <heidi.schellman@oregonstate.edu>
2024-12-19 19:27:14 -0800

    fix bug in test suite

--- utilities/merging/examples/testInteractiveSuite.sh
+++ utilities/merging/examples/testInteractiveSuite.sh
@@ -2,4 +2,4 @@ source examples/MergeLar_hd.sh > examples/MergeLar_hd.sh.log &
 source examples/MergeTar.sh > examples/MergeTar.sh.log &
 source examples/interactiveDirectParentage.sh > examples/interactiveDirectParentage.sh.log &
 source examples/interactiveMerge.sh > examples/interactiveMerge.sh.log &
-source examples/interactiveRun.sh >  examples/interactiveRun.sh &
+source examples/interactiveRun.sh >  examples/interactiveRun.sh.log &
